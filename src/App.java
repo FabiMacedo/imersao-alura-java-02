1937b87d43ca3e58ac03386a5778053644c977d0
FabiMacedo <fabimacedo.oliveira@gmail.com>
2023-03-28 21:02:57 -0300

    Solved challenges

--- src/App.java
+++ src/App.java
@@ -11,6 +11,9 @@ public class App {
         // fazer conexão HTTP e buscar os top 250 filmes
         // top movies -> String url =
         // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+
+        // pegar var de ambiente -> String imdbKey = System.getenv("IMDB_API_KEY");
+
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -38,10 +41,22 @@ public class App {
         // for da lista com 3 elementos
         for (int i = 0; i < 3; i++) {
             Map<String, String> filme = listaDeFilmes.get(i);
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
-            System.out.println();
+            System.out.println("\u001b[1mTítulo:\u001b[0m " + filme.get("title"));
+            System.out.println("\u001b[1mURL da imagem:\u001b[0m " + filme.get("image"));
+            System.out.println("\u001b[37;1m\u001b[45;1mRanking: " + filme.get("imDbRating") + "\u001b[m");
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+            int numeroEstrelinhas = (int) classificacao;
+
+            if (numeroEstrelinhas <= 3) {
+                for (int j = 0; j < numeroEstrelinhas; j++) {
+                    System.out.print("🍅");
+                }
+            } else {
+                for (int s = 1; s <= numeroEstrelinhas; s++) {
+                    System.out.print("⭐");
+                }
+            }
+            System.out.println("\n");
 
         }
     }
